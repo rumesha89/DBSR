0f2970c9009f0c2b7545b06565d2317649e2fbef
Rumesha Ranathunga <ryohani89@gmail.com>
2020-02-08 14:16:50 +0800

    init project

---
+++ src/main/java/com/DBSR/weatherforcast/WeatherforcastApplication.java
@@ -0,0 +1,13 @@
+package com.DBSR.weatherforcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherforcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherforcastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DBSR/weatherforcast/WeatherforcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBSR.weatherforcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherforcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
